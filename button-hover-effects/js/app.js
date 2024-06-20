0feb3f763f042cf4b25b7edfb272d83fadcec703
Abbasi Dev <abbasidev@outlook.com>
2024-06-21 00:27:59 +0330

    updated

--- button-hover-effects/js/app.js
+++ button-hover-effects/js/app.js
@@ -1,4 +1,4 @@
-const linksPreventDefault = document.querySelectorAll(".btnList a");
+const linksPreventDefault = document.querySelectorAll(".btn-list a");
 
 for (let i = 0, n = linksPreventDefault.length; i < n; i++) {
   linksPreventDefault[i].onclick = function (event) {
